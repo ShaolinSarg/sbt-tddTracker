1a3aa643503ddb9eb6a457422d4adc3bc6c9ae31
David Sarginson <David.Sarginson@OpencastSoftware.com>
2016-06-08 16:09:50 +0100

    hopefully making an autoplugin

--- build.sbt
+++ build.sbt
@@ -1,11 +1,5 @@
-val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % Test
-val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.12.5" % Test
-
-resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-
-
 lazy val commonSettings = Seq(
-  organization := "com.sarginsonconsulting",
+  organization := "com.sarginsonconsulting.sbt",
   version := "0.1.0",
   scalaVersion := "2.11.7"
 )
@@ -14,6 +8,5 @@ lazy val root = (project in file(".")).
   settings(commonSettings: _*).
   settings(
     name := "sbt TDD metrics tracker",
-    libraryDependencies ++= Seq(scalaTest, scalaCheck),
-    testListeners += new TddTestListner
+    sbtPlugin := true
   )
---
+++ src/main/scala/com/sarginsonconsulting/sbt/TddTestReporter.scala
@@ -0,0 +1,42 @@
+package com.sarginsonconsulting.sbt
+
+
+import sbt._
+import sbt.testing.Status.{Failure, Error, Success}
+
+
+object TddTestReporterPlugin extends AutoPlugin {
+
+    object autoImport {}
+
+    override lazy val projectSettings = Seq(
+        testListeners += new TddTestReporter
+    )
+
+    override val trigger = AllRequirements
+}
+
+
+class TddTestReporter extends TestsListener {
+
+  var successCount: Int = 0
+  var failureCount: Int = 0
+
+  override def doInit(): Unit = ()
+
+  override def doComplete(finalResult: TestResult.Value): Unit = {
+    println("@@@@@@ THIS WORKS @@@@@@")
+    println(s"there were $successCount successful tests")
+    println(s"there were $failureCount failing tests")
+  }
+
+  override def testEvent(event: TestEvent): Unit = {
+    successCount = successCount + event.detail.count(t => t.status()==Success)
+    failureCount = failureCount + event.detail.count(t => (t.status() == Error) || (t.status()==Failure))
+  }
+
+  override def startGroup(name: String): Unit = ()
+  override def endGroup(name: String, t: Throwable): Unit = ()
+  override def endGroup(name: String, result: TestResult.Value): Unit = ()
+
+}
